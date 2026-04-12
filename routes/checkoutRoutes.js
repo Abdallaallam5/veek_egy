b5bfe0caf8afac8b3c8b64fe9071dad64c016630
Abdallaallam5 <114347981+Abdallaallam5@users.noreply.github.com>
2026-04-12 19:44:31 +0200

    Remove OTP routes from checkoutRoutes
    Removed OTP-related routes from checkout.

--- routes/checkoutRoutes.js
+++ routes/checkoutRoutes.js
@@ -5,7 +5,5 @@ const checkoutController = require('../controllers/checkoutController');
 router.get('/get', checkoutController.getCheckout);
 router.post('/place-order', checkoutController.placeOrder);
 router.get('/calculate', checkoutController.calculate);
-router.post("/checkout/send-otp", checkoutController.sendOtp);
-router.post("/checkout/verify-otp", checkoutController.verifyOtp);
 
 module.exports = router;
