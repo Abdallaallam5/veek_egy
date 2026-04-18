b34a5e3526fb4491bba9f1571a34cf1b69ea478b
Abdallaallam5 <114347981+Abdallaallam5@users.noreply.github.com>
2026-04-18 16:21:09 +0200

    Update multer.js

--- config/multer.js
+++ config/multer.js
@@ -1,15 +1,21 @@
 const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const cloudinary = require("./cloudinary"); // تأكد المسار
+const cloudinary = require("./cloudinary");
 
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
     folder: "veek_uploads",
     allowed_formats: ["jpg", "png", "jpeg", "webp"],
+    resource_type: "image",
   },
 });
 
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024 // 5MB
+  }
+});
 
 module.exports = upload;
