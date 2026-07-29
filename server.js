20a24866966ad4d0c73d4f4f5db67703309e50e3
Abdallaallam5 <114347981+Abdallaallam5@users.noreply.github.com>
2026-07-29 20:59:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(cartCountMiddleware);
 // MongoDB
 
 
-await mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB connected successfully"))
   .catch(err => console.log("MongoDB connection error:", err));
 
